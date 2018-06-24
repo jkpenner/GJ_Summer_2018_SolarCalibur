276c8e015f15592b5bb1671d5c60092058f55fe0
Jacob Penner <jkpenner@live.com>
2018-06-23 21:16:04 -0700

    Initial Player Planet Controller with Asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+    public event System.Action<Asteroid> EventLaunched;
+    public event System.Action<Asteroid> EventReturned;
+
+    [Header("Setup")]
+    public RectTransform targeting;
+    public new Rigidbody rigidbody;
+
+
+    [Header("Orbit")]
+    public float orbit_speed = 6f;
+    public float orbit_radius = 1f;
+
+    [HideInInspector] // Assigned by PlanetController
+    public Transform orbit_target;
+    [HideInInspector] // Assigned by PlanetController
+    public float orbit_angle = 0f;
+    [HideInInspector] // Assigned by PlanetController
+    public float orbit_direction = 1f;
+
+    [Header("Launch")]
+    public float launch_speed = 15f;
+    public float return_distance = 12f;
+
+    [Header("Charging")]
+    public float charge_time = 1f;
+    public float min_charge_mod = 1f;
+    public float max_charge_mod = 3f;
+
+    [NonSerialized]
+    public float active_charge_mod = 1f;
+    private Coroutine chargeCoroutine = null;
+
+    [HideInInspector]
+    public bool isActive = false;
+    private bool isOrbitting = true;
+    private bool isReturning = false;
+    private float returningPercent = 0f;
+
+    public bool IsOrbitting { get { return isOrbitting; } }
+
+    public float OrbitSpeed {
+        get { return isActive ? orbit_speed : orbit_speed * 0.75f; }
+    }
+
+    public float OrbitRadius {
+        get { return isActive ? orbit_radius : orbit_radius * 0.75f; }
+    }
+
+
+    public Vector3 TargetOrbitPosition {
+        get {
+            Vector3 toPosition = new Vector3(Mathf.Cos(orbit_angle), 0f, Mathf.Sin(orbit_angle));
+            if (orbit_target != null) {
+                return orbit_target.position + (toPosition * OrbitRadius);
+            }
+            return toPosition * OrbitRadius;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Hit " + collision.transform.name);
+
+        isReturning = true;
+        returningPercent = 1f;
+    }
+
+    private void Awake() {
+        this.rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody == null) {
+            rigidbody = gameObject.AddComponent<Rigidbody>();
+        }
+
+        rigidbody.isKinematic = true;
+    }
+
+
+    public void Update() {
+
+        targeting.gameObject.SetActive(isActive && isOrbitting);
+
+
+        var colliders = GetComponentsInChildren<Collider>();
+        foreach (var collider in colliders) {
+            collider.enabled = isActive || isOrbitting == false;
+        }
+
+
+        if (isOrbitting) {
+            float percent_of_cir = (OrbitSpeed * active_charge_mod * Time.deltaTime)
+                / (2.0f * Mathf.PI * OrbitRadius);
+            float move_in_degrees = 360f * percent_of_cir * orbit_direction;
+
+
+            // Modify the oribit angle by how much we moved across the circumference
+            this.orbit_angle += (move_in_degrees * Mathf.Deg2Rad);
+
+            // Determine the vector towards the position based off the current angle
+            Vector3 toPosition = new Vector3(Mathf.Cos(orbit_angle), 0f, Mathf.Sin(orbit_angle));
+
+            // Get the forward vector for the asteroid, adjusted for move direction
+            Vector3 forward = Vector3.Cross(toPosition, Vector3.up) * orbit_direction;
+
+            // Update the rigidbody's position and rotation values
+            rigidbody.MovePosition(orbit_target.position + (toPosition * OrbitRadius));
+            rigidbody.MoveRotation(Quaternion.LookRotation(forward));
+        } else {
+
+
+
+            float move_amount = launch_speed * active_charge_mod * Time.deltaTime;
+
+            CheckIfAsteroidNeedsToReturn();
+
+            Vector3 next_moved_position = rigidbody.position + transform.forward * move_amount;
+
+            float distToTarget = Vector3.Distance(TargetOrbitPosition, rigidbody.position);
+
+            Vector3 next_return_position;
+            if (Mathf.Max(distToTarget - move_amount, 0f) == 0) {
+                next_return_position = TargetOrbitPosition;
+            } else {
+                next_return_position = rigidbody.position +
+                    ((TargetOrbitPosition - rigidbody.position).normalized * move_amount);
+            }
+
+            next_return_position = Vector3.Lerp(next_moved_position, next_return_position, returningPercent);
+            rigidbody.MovePosition(next_return_position);
+
+            if (TargetOrbitPosition == rigidbody.position) {
+                OnReturned();
+            }
+        }
+    }
+
+    private void OnReturned() {
+        isReturning = false;
+        isOrbitting = true;
+        returningPercent = 0f;
+        active_charge_mod = min_charge_mod;
+
+        if (EventReturned != null) {
+            EventReturned.Invoke(this);
+        }
+    }
+
+    private void CheckIfAsteroidNeedsToReturn() {
+        // Asteroid is already returning exit early
+        if (isReturning) return;
+
+        if (Vector3.Distance(this.rigidbody.position, TargetOrbitPosition) > return_distance) {
+            Debug.Log("Returning");
+            isReturning = true;
+            StartCoroutine(StartReturning(1f));
+        }
+    }
+
+    private IEnumerator StartReturning(float transition_time) {
+        if (transition_time <= 0f) yield break;
+
+        returningPercent = 0f;
+
+        float counter = 0f;
+        while (counter < transition_time) {
+            counter += Time.deltaTime;
+
+            returningPercent = counter / transition_time;
+
+            yield return null;
+        }
+        returningPercent = 1f;
+    }
+
+    public void StartCharge() {
+        if (chargeCoroutine != null) {
+            StopCoroutine(chargeCoroutine);
+            chargeCoroutine = null;
+        }
+        chargeCoroutine = StartCoroutine(StartCharging());
+    }
+
+    private IEnumerator StartCharging() {
+        if (charge_time <= 0f) yield break;
+
+        active_charge_mod = min_charge_mod;
+
+        float counter = 0f;
+        while (counter < charge_time && isOrbitting) {
+            counter = Mathf.Clamp(counter + Time.deltaTime, 0f, charge_time);
+
+            active_charge_mod = ((max_charge_mod - min_charge_mod) * (counter / charge_time)) + min_charge_mod;
+            yield return null;
+        }
+
+        chargeCoroutine = null;
+    }
+
+    public void Launch() {
+        if (isOrbitting) {
+            isOrbitting = false;
+
+            if (chargeCoroutine != null) {
+                StopCoroutine(chargeCoroutine);
+                chargeCoroutine = null;
+            }
+
+            // When launching the asteroid from one side of the planet we want to 
+            // return it to the opposite side. Update the orbit angle
+            this.orbit_angle += (180f * Mathf.Deg2Rad);
+
+            // Trigger the asteroid launch event
+            if (this.EventLaunched != null) {
+                this.EventLaunched.Invoke(this);
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(TargetOrbitPosition, 0.5f);
+    }
+}
---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour {
+    [Header("Movement")]
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float moveDistanceMax = 1.8f;
+
+    [Header("Asteroids")]
+    [SerializeField]
+    private Asteroid asteroidPrefab = null;
+    [SerializeField]
+    private int asteroidCount = 1;
+
+    // List containing all asteroids controlled by this planet
+    private List<Asteroid> asteroids;
+
+    // Queue of asteroids available to be launched
+    private Queue<Asteroid> asteroidQueue;
+
+    void Start() {
+        asteroids = new List<Asteroid>();
+        asteroidQueue = new Queue<Asteroid>();
+        for (int i = 0; i < asteroidCount; i++) {
+            asteroids.Add(Instantiate(asteroidPrefab));
+            asteroids[i].orbit_target = this.transform;
+            asteroids[i].orbit_angle = ((float)i / asteroidCount) * 360f;
+            //asteroids[i].orbit_radius = 1f;
+            asteroids[i].orbit_direction = UnityEngine.Random.Range(0, 2) == 0 ? 1 : -1;
+
+            //asteroids[i].transform.SetParent(this.transform, false);
+            asteroids[i].EventLaunched += OnAsteroidLauncehd;
+            asteroids[i].EventReturned += OnAsteroidReturned;
+
+
+            asteroidQueue.Enqueue(asteroids[i]);
+        }
+    }
+
+    private void OnAsteroidLauncehd(Asteroid asteroid) {
+        asteroid.isActive = false;
+    }
+
+    private void OnAsteroidReturned(Asteroid asteroid) {
+        asteroidQueue.Enqueue(asteroid);
+    }
+
+    void Update() {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.localPosition += input * moveSpeed * Time.deltaTime;
+        if (Mathf.Abs(transform.localPosition.x) > moveDistanceMax && moveDistanceMax > 0f) {
+            float new_x = (transform.localPosition.x >= 0 ? 1 : -1) * moveDistanceMax;
+
+            transform.localPosition = new Vector3(new_x, 0f, 0f);
+        }
+
+
+
+
+        if (asteroidQueue.Count > 0) {
+            asteroidQueue.Peek().isActive = true;
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (asteroidQueue.Count > 0) {
+                asteroidQueue.Peek().StartCharge();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            if (asteroidQueue.Count > 0) {
+                asteroidQueue.Dequeue().Launch();
+            }
+
+
+
+            //ShootAvalibleAsteroid();
+        }
+    }
+
+    private void ShootAvalibleAsteroid() {
+        for (int i = 0; i < asteroids.Count; i++) {
+            if (asteroids[i].IsOrbitting)
+                continue;
+
+            asteroids[i].isActive = false;
+            asteroids[i].Launch();
+            break;
+        }
+    }
+}
