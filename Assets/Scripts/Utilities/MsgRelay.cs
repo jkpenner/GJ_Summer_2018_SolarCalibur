211cf5e3e467bb6af6d131ea6ff0e797011b224d
Jacob Penner <jkpenner@live.com>
2018-06-25 10:53:58 -0800

    Add Planet and Player events to MsgRelay

--- Assets/Scripts/Utilities/MsgRelay.cs
+++ Assets/Scripts/Utilities/MsgRelay.cs
@@ -19,6 +19,38 @@ public class MsgRelay : Singleton<MsgRelay> {
     private event System.Action _eventGameResume;
     private event System.Action _eventGameComplete;
 
+    /// <summary>
+    /// Triggers when the game logic should start
+    /// </summary>
+    static public event System.Action EventGameStart {
+        add { Instance._eventGameStart += value; }
+        remove { Instance._eventGameStart -= value; }
+    }
+
+    /// <summary>
+    /// Triggers when the game logic should pause
+    /// </summary>
+    static public event System.Action EventGamePause {
+        add { Instance._eventGamePause += value; }
+        remove { Instance._eventGamePause -= value; }
+    }
+
+    /// <summary>
+    /// Triggers when the game logic should resume
+    /// </summary>
+    static public event System.Action EventGameResume {
+        add { Instance._eventGameResume += value; }
+        remove { Instance._eventGameResume -= value; }
+    }
+
+    /// <summary>
+    /// Triggers when the game is complete. End of a fight
+    /// </summary>
+    static public event System.Action EventGameComplete {
+        add { Instance._eventGameComplete += value; }
+        remove { Instance._eventGameComplete -= value; }
+    }
+
     static public void TriggerGameStart() {
 #if SHOW_DEBUG_LOGS
         Debug.Log("Game Start Triggered");
@@ -58,37 +90,103 @@ public class MsgRelay : Singleton<MsgRelay> {
             Instance._eventGameComplete.Invoke();
         }
     }
+    #endregion
+
+    #region Planet Events
+    private System.Action<Planet> _eventPlanetSpawned;
+    private System.Action<Planet> _eventPlanetDamaged;
+    private System.Action<Planet> _eventPlanetDestroyed;
+    private System.Action<Planet> _eventPlayerAssigned;
+    private System.Action<Planet> _eventPlayerUnassigned;
 
     /// <summary>
-    /// Triggers when the game logic should start
+    /// Triggers when the game is complete. End of a fight
     /// </summary>
-    static public event System.Action EventGameStart {
-        add { Instance._eventGameStart += value; }
-        remove { Instance._eventGameStart -= value; }
-    }
-
-    /// <summary>
-    /// Triggers when the game logic should pause
-    /// </summary>
-    static public event System.Action EventGamePause {
-        add { Instance._eventGamePause += value; }
-        remove { Instance._eventGamePause -= value; }
-    }
-
-    /// <summary>
-    /// Triggers when the game logic should resume
-    /// </summary>
-    static public event System.Action EventGameResume {
-        add { Instance._eventGameResume += value; }
-        remove { Instance._eventGameResume -= value; }
+    static public event System.Action<Planet> EventPlanetSpawned {
+        add { Instance._eventPlanetSpawned += value; }
+        remove { Instance._eventPlanetSpawned -= value; }
     }
 
     /// <summary>
     /// Triggers when the game is complete. End of a fight
     /// </summary>
-    static public event System.Action EventGameComplete {
-        add { Instance._eventGameComplete += value; }
-        remove { Instance._eventGameComplete -= value; }
+    static public event System.Action<Planet> EventPlanetDamaged {
+        add { Instance._eventPlanetDamaged += value; }
+        remove { Instance._eventPlanetDamaged -= value; }
     }
-#endregion
+
+    /// <summary>
+    /// Triggers when the game is complete. End of a fight
+    /// </summary>
+    static public event System.Action<Planet> EventPlanetDestroyed {
+        add { Instance._eventPlanetDestroyed += value; }
+        remove { Instance._eventPlanetDestroyed -= value; }
+    }
+
+    /// <summary>
+    /// Triggers when a planet's isplayer property is set to true
+    /// </summary>
+    static public event System.Action<Planet> EventPlayerAssigned {
+        add { Instance._eventPlayerAssigned += value; }
+        remove { Instance._eventPlayerAssigned -= value; }
+    }
+
+    /// <summary>
+    /// Triggers when a planet's isplayer property is set to false from true
+    /// </summary>
+    static public event System.Action<Planet> EventPlayerUnassigned {
+        add { Instance._eventPlayerUnassigned += value; }
+        remove { Instance._eventPlayerUnassigned -= value; }
+    }
+
+    static public void TriggerPlanetSpawned(Planet planet) {
+#if SHOW_DEBUG_LOGS
+        Debug.LogFormat("[{0}]: Planet Spawned", planet.name);
+#endif
+
+        if (Exists && Instance._eventPlanetSpawned != null) {
+            Instance._eventPlanetSpawned.Invoke(planet);
+        }
+    }
+
+    static public void TriggerPlanetDamaged(Planet planet) {
+#if SHOW_DEBUG_LOGS
+        Debug.LogFormat("[{0}]: Planet Damaged", planet.name);
+#endif
+
+        if (Exists && Instance._eventPlanetDamaged != null) {
+            Instance._eventPlanetDamaged.Invoke(planet);
+        }
+    }
+
+    static public void TriggerPlanetDestroyed(Planet planet) {
+#if SHOW_DEBUG_LOGS
+        Debug.LogFormat("[{0}]: Planet Destroyed", planet.name);
+#endif
+
+        if (Exists && Instance._eventPlanetDestroyed != null) {
+            Instance._eventPlanetDestroyed.Invoke(planet);
+        }
+    }
+
+    static public void TriggerPlayerAssigned(Planet planet) {
+#if SHOW_DEBUG_LOGS
+        Debug.LogFormat("[{0}]: Assigned as player", planet.name);
+#endif
+
+        if (Exists && Instance._eventPlayerAssigned != null) {
+            Instance._eventPlayerAssigned.Invoke(planet);
+        }
+    }
+
+    static public void TriggerPlayerUnassigned(Planet planet) {
+#if SHOW_DEBUG_LOGS
+        Debug.LogFormat("[{0}]: Unassigned as player", planet.name);
+#endif
+
+        if (Exists && Instance._eventPlayerUnassigned != null) {
+            Instance._eventPlayerUnassigned.Invoke(planet);
+        }
+    }
+    #endregion
 }
