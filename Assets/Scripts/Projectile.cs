9bd36d5938495a7c1865308382fa233e7f110a5e
Andrew Hair <andrew.hair@akqa.com>
2018-06-25 16:34:52 -0700

    Renamed some stuff. Added OnCollisionEnter to Rocket and EnemyPlanet Adds Damage to Rocket and Health to EnemyPlanet.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float VelocityMin = 7.0f;  //The min velocity this projectile will move at 
+    public float VelocityMax = 12.0f; //The max velocity this projectile will move at 
+
+    private float Velocity;           //The projectile's velocity
+
+    void Start ()
+    {
+        // Add velocity
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        if(rigidBody != null)
+        {
+            Velocity = Random.Range(VelocityMin, VelocityMax);
+            rigidBody.velocity = transform.forward * Velocity;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || (collision.gameObject.CompareTag("Asteroid")))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
