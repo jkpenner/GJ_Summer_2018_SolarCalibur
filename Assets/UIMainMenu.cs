5aa7fa88d2e90f7122115a7b81ec80724e84271a
Jacob Penner <jkpenner@live.com>
2018-06-28 10:33:51 -0700

    Add Placeholder ui to MainMenu to start new Game

---
+++ Assets/UIMainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMainMenu : MonoBehaviour {
+    public Button new_game;
+
+    private void OnEnable() {
+        new_game.onClick.AddListener(OnNewGameClick);
+    }
+
+    private void OnDisable() {
+        new_game.onClick.RemoveListener(OnNewGameClick);
+    }
+
+    private void OnNewGameClick() {
+        if (DataManager.Exists) {
+            DataManager.SetToFirstLevel();
+        }
+        SceneManager.LoadScene("Gameplay");
+    }
+}
