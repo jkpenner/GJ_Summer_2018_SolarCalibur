e056e73397f99c663987113c1b2c9046ca159bf3
Jacob Penner <jkpenner@live.com>
2018-06-28 12:00:00 -0700

    Add Utility script to override main camera is level scenes

---
+++ Assets/Scripts/Utilities/MainCameraOverride.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraOverride : MonoBehaviour {
+    private Camera mainCamera = null;
+
+    private void OnEnable() {
+        if (Camera.main != null) {
+            mainCamera = Camera.main;
+            mainCamera.enabled = false;
+        }
+    }
+
+    private void OnDisable() {
+        if (mainCamera != null) {
+            mainCamera.enabled = true;
+        }
+    }
+
+    void Update() {
+        if (mainCamera != null) {
+            this.transform.rotation = mainCamera.transform.rotation;
+            this.transform.position = mainCamera.transform.position;
+        }
+    }
+}
