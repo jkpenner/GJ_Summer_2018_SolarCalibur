25b27b8056f4b7c9cdaf2e95fb7892a35bf64af5
Scott Price <scottprice7@gmail.com>
2018-06-28 19:13:27 -0700

    making menu work better

---
+++ Assets/MenuParticlesTop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuParticlesTop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start (){
+		Vector3 pos = Vector3.zero;
+		pos.y = Screen.height + 20;
+		transform.position = pos;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
